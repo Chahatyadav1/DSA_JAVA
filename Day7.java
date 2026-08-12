463fffb03ad5705d5a4f24f5753ffbdb2b09f5f1
Chahat Yadav <chahatyadav855@gmail.com>
2026-08-12 23:47:38 +0530

    add new code

--- Day7.java
+++ Day7.java
@@ -142,3 +142,190 @@
 //         System.out.println(result);
 //     }
 // }
+
+// import java.util.ArrayList;
+// import java.util.List;
+
+// /**
+//  * Day7
+//  */
+// public class Day7 {
+//     public static void solve(int[] candidates, int target,int sum,List<List<Integer>> result,List<Integer> ans,int index){
+//         if(sum==target){
+//             result.add(new ArrayList(ans));
+//             sum=0;
+//             return;
+//         }
+//         if(index>=candidates.length || sum>target){
+//             return;
+//         }
+//         ans.add(candidates[index]);
+//         solve(candidates,target,sum+candidates[index],result,ans,index);
+//         ans.remove(ans.size()-1);
+//         solve(candidates,target,sum,result,ans,index+1);
+//     }
+//     public static void main(String[] args) {
+//         int[] candidates ={2,3,6,7};
+//         int target=7;
+//         List<List<Integer>> result=new ArrayList<>();
+//         List<Integer> ans = new ArrayList<>();
+//         solve(candidates,target,0,result,ans,0);
+//         System.out.println(result);
+//     }
+// }
+/**
+ * Day7
+ */
+// public class Day7 {
+
+//     public static void main(String[] args) {
+//         // StringBuilder sb=new StringBuilder();
+//         // // String.valueOf(sb);
+//         // String h="12";
+//         // char a=h.charAt(0);
+//     }
+// }
+
+/**
+ * Day7
+//  */
+// public class Day7 {
+//     static int quicksort(int[] nums,int l,int r){
+//         int pivot=nums[r];
+//         int j=r-1;
+//         int i=l;
+//         while(i<=j){
+//             if(pivot>nums[i]){
+//                 i++;
+//             }else{
+//                 int temp=nums[i];
+//                 nums[i]=nums[j];
+//                 nums[j]=temp;
+//                 j--;
+//             }
+//         }
+//         int temp=nums[i];
+//         nums[i]=nums[r];
+//         nums[r]=temp;
+//         return i;
+//     }
+//     static void sort(int[] nums,int l,int r){
+//         if (l < r) {
+
+//             int pivotIndex = quicksort(nums, l, r);
+
+//             sort(nums, l, pivotIndex - 1);
+
+//             sort(nums, pivotIndex + 1, r);
+//         }
+//     }
+
+//     public static void main(String[] args) {
+//         int[] arr={1,9,4,6,2,5};
+//         sort(arr, 0, arr.length-1);
+//         for(int i: arr){
+//             System.out.println(i);
+//         }
+//     }
+// }
+
+/**
+ * Day7
+ */
+// public class Day7 {
+
+//     public static void main(String[] args) {
+//         String s="ababa";
+//         int j=s.length()-1;
+//         for(int i=0;i<s.length()/2;i++){
+//             if(s.charAt(i)!=s.charAt(j)){
+//                 System.out.println("not palindrone");
+//             }
+//             j--;
+//         }
+//         System.out.println("Palindrome");
+//     }
+// }
+
+// import java.util.ArrayList;
+// import java.util.Collection;
+// import java.util.Collections;
+// import java.util.HashMap;
+// import java.util.List;
+// import java.util.Map;
+
+// /**
+//  * Day7
+//  */
+// public class Day7 {
+
+//     public static void main(String[] args) {
+//         int[] arr={4, 6, 2, 6, 4, 4, 2};
+//         HashMap<Integer, Integer> map = new HashMap<>();
+
+//         // frequency
+//         for (int x : arr) {
+//             map.put(x, map.getOrDefault(x, 0) + 1);
+//         }
+
+//         // sort elements by frequency
+//         List<Integer> list = new ArrayList<>(map.keySet());
+
+//         list.sort((a, b) -> map.get(b) - map.get(a));
+//         int index=0;
+//         // System.out.println(list);
+//         for(int i=0;i<list.size();i++){
+//             int num=list.get(i);
+//             int freq=map.get(list.get(i));
+//             for(int j=0;j<freq;j++){
+//                 arr[index]=num;
+//                 index++;
+//             }
+//         }
+//         for(int k:arr){
+//             System.out.println(k);
+//         }
+//     }
+// }
+
+/**
+ * Day7
+ */
+/**
+ * Day7
+ */
+// public class Day7 {
+
+//     public static void main(String[] args) {
+//         int[] arr={0,2,3,4,5,6,7,8,1};
+//         int sum=9*(10/2);
+//         int count=0;
+//         for(int i:arr){
+//             count+=i;
+//         }
+//         System.out.println(sum-count);
+//     }
+// }
+
+/**
+ * Day7
+ */
+// public class Day7 {
+
+//     public static void main(String[] args) {
+//         int[] arr = { 1, 2, 2, 2, 2, 3, 3, 4 };
+//         int i = 0;
+//         int j = 0;
+//         while (j < arr.length) {
+//             arr[i]=arr[j];
+//             while (j < arr.length - 1 && arr[j] == arr[j + 1]) {
+//                 j++;
+//             }
+//             i++;
+//             j++;
+//         }
+//         for(int k:arr){
+//             System.out.println(k);
+//         }
+//     }
+// }
