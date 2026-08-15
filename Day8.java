223d23ec44da1144218f81071ea191f939cbd8eb
Chahat Yadav <chahatyadav855@gmail.com>
2026-08-15 23:42:32 +0530

    add day8 data

--- Day8.java
+++ Day8.java
@@ -149,3 +149,94 @@ import java.util.*;
 //     }
 // }
 
+/**
+ * Day8
+//  */
+// public class Day8 {
+//     static void merge(int[] arr,int l,int mid,int r){
+//         int[] temp= new int[r-l+1];
+//         int i=l;
+//         int j=mid+1;
+//         int k=0;
+//         while(i<=mid && j<=r){
+//             if(arr[i]<arr[j]){
+//                 temp[k]=arr[i];
+//                 i++;
+//             }
+//             else{
+//                 temp[k]=arr[j];
+//                 j++;
+//             }
+//             k++;
+//         }
+//         while(i<=mid){
+//             temp[k]=arr[i];
+//             k++;i++;
+//         }
+//         while(j<=r){
+//             temp[k]=arr[j];
+//             k++;j++;
+//         }
+//         for(int p=0;p<temp.length;p++){
+//             arr[l+p]=temp[p];
+//         }
+//     }
+//     static void mergesort(int[] arr, int l,int r){
+//         if(l<r){
+//             int mid=l+(r-l)/2;
+//             mergesort(arr, l, mid);
+//             mergesort(arr,mid+1,r);
+//             merge(arr, l, mid, r);
+//         }
+//     }
+//     public static void main(String[] args) {
+//         int [] arr={1,6,3,8,9,3,4};
+//         mergesort(arr, 0, arr.length-1);
+//         for(int i:arr){
+//             System.out.println(i);
+//         }
+//     }
+// }
+
+/**
+ * Day8
+//  */
+// public class Day8 {
+//     static int partition(int[] arr, int l, int r) {
+//             int pivot = r;
+//             int i = l;
+//             int j = r - 1;
+//             while (i <=  j) {
+//                 if (arr[i] > arr[pivot]) {
+//                     int temp = arr[j];
+//                     arr[j] = arr[i];
+//                     arr[i] = temp;
+//                     j--;
+//                 } else {
+//                     i++;
+//                 }
+//             }
+//             int temp=arr[i];
+//             arr[i]=arr[pivot];
+//             arr[pivot]=temp;
+        
+//         return i;
+
+//     }
+
+//     public static void quicksort(int[] arr, int l, int r) {
+//         if(l<r){
+//             int p=partition(arr, l, r);
+//             quicksort(arr, l, p-1);
+//             quicksort(arr, p+1, r);
+//         }
+//     }
+
+//     public static void main(String[] args) {
+//         int[] arr = { 1, 6, 3, 8, 9, 3, 4 };
+//         quicksort(arr, 0, arr.length-1);
+//         for(int i:arr){
+//             System.out.println(i);
+//         }
+//     }
+// }
